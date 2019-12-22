2e34f5f4dfa461ca5ea5e83c906c774953d2a22d
J Dennis Chapman <j.dennis.chapman@comcast.net>
2019-12-22 09:30:28 -0500

    Routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DockablesComponent } from './components/dockables/dockables.component';
 
 
 const routes: Routes = [
+  {path: '', component: DockablesComponent},
   {path: '/', component: DockablesComponent},
   {path: 'dockables-ng', component: DockablesComponent}
 ];
