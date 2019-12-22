6bbd0f2572a0cf512c914e00edfff81f3b1603e0
J Dennis Chapman <j.dennis.chapman@comcast.net>
2019-12-22 13:27:49 -0500

    router: revert back to app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DockablesComponent } from './components/dockables/dockables.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
-  {path: '', component: DockablesComponent},
-  {path: './', component: DockablesComponent},
-  {path: 'dockables-ng', component: DockablesComponent}
+  {path: '', component: AppComponent},
+  {path: './', component: AppComponent},
+  {path: 'dockables-ng', component: AppComponent}
 ];
 
 @NgModule({
